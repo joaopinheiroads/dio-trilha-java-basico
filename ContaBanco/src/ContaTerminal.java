a06101333102e3482060a48606ce6f2dc9d31e25
João Lucas Gomes Pinheiro <joaopinheiro.ads@gmail.com>
2024-10-27 11:59:57 -0300

    Projeto finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,50 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!!!");
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+    
+
+        String nomeCliente="";
+        String agencia="";
+        int numero;
+        double saldo;
+
+        System.out.println("Digite seu nome:");
+        nomeCliente = scanner.nextLine();
+        System.out.println("Digite o número da sua conta:");
+        numero = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Digite o número da sua agência:");
+        agencia = scanner.nextLine();
+        System.out.println("Digite o seu saldo:");
+        saldo = scanner.nextDouble();
+        scanner.close();
+
+
+        
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + 
+        ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+
+
+        
+
+
+
+        
+
+
+
+
+        
+
+
+        
+
+
+
+        
     }
 }
